235382e09145e53a22d06c4efba5db3ccd98e662
David Duong <75517589+davidduonggg@users.noreply.github.com>
2026-06-16 21:06:26 -0700

    Add: longest-repeating-substring-with-replacement - submission-6

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-6.py
@@ -0,0 +1,28 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # given a string s 
+        # choose up to k characters of the string and replace them with any other character
+        # after performing at most k replacements, return the length of the longest substring
+        # which contains only one distinct character
+
+        chars = defaultdict(int)
+        maxLen = 0
+        maxChar = 0
+
+        # the question is, how do we keep track of the maximum from the hashmap
+        L = 0
+        for R in range(len(s)):
+            chars[s[R]] += 1
+            maxChar = max(maxChar, chars[s[R]])
+
+            while (R - L + 1) - maxChar > k:
+                chars[s[L]] -= 1
+                L += 1
+
+            maxLen = max(maxLen, R - L + 1)
+
+        return maxLen
+            
+
+
+
