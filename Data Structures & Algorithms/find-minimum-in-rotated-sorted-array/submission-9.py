7e51d10df9d36d061883dcf911e96caf3c14f222
David Duong <75517589+davidduonggg@users.noreply.github.com>
2026-08-01 18:58:45 -0700

    Add: find-minimum-in-rotated-sorted-array - submission-9

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-9.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        # given an array of length n
+        # originally sorted in ascending order
+        # it has now been rotated between 1 and n times
+        
+        # all elements in the rotated sorted array are unique
+        
+        L, R = 0, len(nums) - 1
+
+        while L < R:
+            # return the minimum element
+            mid = (L + R) // 2
+
+            if nums[mid] > nums[R]: # this is the unsorted half
+                L = mid + 1
+            else:
+                R = mid
+
+        return nums[R]
