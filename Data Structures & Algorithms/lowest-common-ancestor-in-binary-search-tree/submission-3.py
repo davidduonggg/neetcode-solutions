97b750b94e83ad542fd036c755dab5e71adb4875
David Duong <75517589+davidduonggg@users.noreply.github.com>
2026-08-04 18:58:30 -0700

    Add: lowest-common-ancestor-in-binary-search-tree - submission-3

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-3.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        # all node vals are unique
+        # lowest node in a tree that has both p and q as descendants
+        # ancestor is a descendant of itself
+
+        # p != q, p and q will exist
+
+        # brute force: for every node n, we check if p and q are descendants
+        # and then we check. thats O(n^2)
+        # we want a solution that is O(n), we check every node once
+
+        # my initial thoughts are just to dfs, and if we find the node
+        # then return True, and then at the first instance of both returning true we set val to true?
+
+        # all values are unique, we don't have to worry about repeating p and q
+
+        # we also exploit that its a BST
+
+        if not root: return None
+
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else: return root
+
+
+
