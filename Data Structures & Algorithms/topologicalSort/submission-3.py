d12e9372023232c846e1a9d1df0837e1fa3d1ced
David Duong <75517589+davidduonggg@users.noreply.github.com>
2026-08-19 16:57:28 -0700

    Add: topologicalSort - submission-3

---
+++ Data Structures & Algorithms/topologicalSort/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def topologicalSort(self, n: int, edges: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        indegree = [0] * n
+
+        for a, b in edges:
+            graph[a].append(b)
+            indegree[b] += 1
+
+        q = deque(i for i in range(n) if indegree[i] == 0)
+
+        res = [] # ordering
+
+        while q:
+            node = q.popleft()
+            
+            for nbr in graph[node]:
+                indegree[nbr] -= 1
+
+                if not indegree[nbr]:
+                    q.append(nbr)
+
+            res.append(node)
+
+        return res if len(res) == n else []
