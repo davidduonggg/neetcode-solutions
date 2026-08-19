7e3b3952b48b4c6e76988d24ec776ebb446df73a
David Duong <75517589+davidduonggg@users.noreply.github.com>
2026-08-19 14:00:38 -0700

    Add: unionFind - submission-7

---
+++ Data Structures & Algorithms/unionFind/submission-7.py
@@ -0,0 +1,40 @@
+class UnionFind:
+    def __init__(self, n: int):
+        # every node is its own parent at first
+        self.par = [i for i in range(n)]
+        self.size = [1] * n
+        self.components = n
+        
+
+    def find(self, x: int) -> int:
+        while x != self.par[x]:
+            self.par[x] = self.par[self.par[x]]
+            x = self.par[x]
+
+        return x
+        
+
+    def isSameComponent(self, x: int, y: int) -> bool:
+        return self.find(x) == self.find(y)
+
+    def union(self, x: int, y: int) -> bool:
+        u, v = self.find(x), self.find(y)
+        if u == v: return False
+
+        # union by size
+        if self.size[u] < self.size[v]:
+            self.par[u] = v
+            self.size[v] += self.size[u]
+        else:
+            self.par[v] = u
+            self.size[u] += self.size[v]
+
+        self.components -= 1
+        
+
+        return True
+
+
+
+    def getNumComponents(self) -> int:
+        return self.components
